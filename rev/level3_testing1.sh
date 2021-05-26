f53962c17f49a80703774f93dae930c9cbe4f000
LinHuiqing <huiqing_lin@mymail.sutd.edu.sg>
2021-05-26 17:00:19 +0000

    rev lvl 3 and 4 testing

---
+++ rev/level3_testing1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# EXPECTED_RESULT=e8fceef7ebf5ffe7
+# after padding (2 characters) => 6161e8fceef7ebf5ffe7
+# element-wise xor value = 0x85 => for 10 chars = 85858585858585858585
+# after padding and xor => e4e46d796b726e707a62
+
+echo $oxolv
+export oxolv=$(echo "e4e46d796b726e707a62" | xxd -p -r)
+exec /challenges/rev/level3_testing1
---
+++ rev/level4_testing1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# EXPECTED_RESULT=02 09 0a 10 0e 1b
+# after padding (4 chars cos "lseek(rax_6, 4, 0)") => 6161616102090a100e1b
+# element-wise xor value = 0x78 => for 10 chars = 78787878787878787878
+# after padding and xor => 191919197a7172687663
+
+echo $(echo "191919197a7172687663" | xxd -p -r) > vecya
+exec /challenges/rev/level4_testing1
