f9e5e011fb270153e109f5cb55e70eb82c876500
LinHuiqing <huiqing_lin@mymail.sutd.edu.sg>
2021-05-26 16:09:14 +0000

    rev lvl5_teaching

---
+++ rev/level5_teaching1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# EXPECTED_RESULT after mangling: 6d 66 72 65 77 76 = mfrewv
+# swap indexes 1 and 3 => merfwv
+# swap indexes 0 and 4 => werfmv
+
+echo "____werfmv" > rzhhb # skip 4 bytes
+exec /challenges/rev/level5_teaching1
